83f50108907391c385db07d7a1d93edec37d98a0
Bogstag <8104765+Bogstag@users.noreply.github.com>
2023-11-28 23:16:05 +0100

    got theme settings to set

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,26 +1,32 @@
-import { defineUserConfig } from "vuepress";
+import { defaultTheme, defineUserConfig } from "vuepress";
 
 export default defineUserConfig({
 	lang: "sv-SE",
 	title: "Brunfilm",
-	description: "brunfilms homepage",
+	description: "brunfilms hemsida",
 
-	defaulttheme: {
+	theme: defaultTheme({
+		// default theme config
 		logo: "/images/BrunfilmLogo.jpg",
 		logoDark: "/images/BrunfilmLogo.jpg",
+		sidebar: false,
+		lastUpdated: false,
+		contributors: false,
 		navbar: [
 			// NavbarItem
 			{
-				text: "Foo",
-				link: "/foo/",
+				text: "Hem",
+				link: "/",
+			},
+			{
+				text: "Om",
+				link: "/Om",
 			},
 			// NavbarGroup
-			{
-				text: "Group",
-				children: ["/group/foo.md", "/group/bar.md"],
-			},
-			// string - page file path
-			"/bar/README.md",
+			// {
+			// 	text: "Om",
+			// 	children: ["/group/foo.md", "/group/bar.md"],
+			// }
 		],
-	},
+	}),
 });
